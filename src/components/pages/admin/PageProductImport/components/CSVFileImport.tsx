0e9e3f9278da0d82709bc2e42e8a1cfd1b0a8274
Alex <vorobev.profi@gmail.com>
2020-11-29 12:26:17 +0300

    TASK-7: Basic auth implementation

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,70 +1,72 @@
-import React, {useState} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import axios from 'axios';
+import axios from "axios";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   content: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(3, 0, 3),
-  },
+    padding: theme.spacing(3, 0, 3)
+  }
 }));
 
 type CSVFileImportProps = {
-  url: string,
-  title: string
+  url: string;
+  title: string;
 };
 
-export default function CSVFileImport({url, title}: CSVFileImportProps) {
+export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const classes = useStyles();
   const [file, setFile] = useState<any>();
   const [uploadUrl, setUploadUrl] = useState<any>();
 
   const createFile = (file: any) => {
-    let reader = new FileReader()
+    let reader = new FileReader();
     reader.onload = (e: any) => {
       console.log(e.target.result);
       setFile(e.target.result);
-    }
-    reader.readAsDataURL(file)
+    };
+    reader.readAsDataURL(file);
   };
 
   const onFileChange = (e: any) => {
     console.log(e);
-    let files = e.target.files || e.dataTransfer.files
-    if (!files.length) return
-    createFile(files[0])
+    let files = e.target.files || e.dataTransfer.files;
+    if (!files.length) return;
+    createFile(files[0]);
   };
 
   const removeFile = () => {
-    setFile('');
+    setFile("");
   };
 
   const uploadFile = async (e: any) => {
-      // Get the presigned URL
-      const response = await axios({
-        method: 'GET',
-        url
-      })
-      console.log('Response: ', response.data)
-      console.log('Uploading: ', file)
-      let binary = atob(file.split(',')[1])
-      let array = []
-      for (var i = 0; i < binary.length; i++) {
-        array.push(binary.charCodeAt(i))
+    // Get the presigned URL
+    const response = await axios({
+      method: "GET",
+      url,
+      headers: {
+        Authorization: "Basic " + localStorage.getItem("authorization_token")
       }
-      let blobData = new Blob([new Uint8Array(array)], {type: 'text/plain'})
-      console.log('Uploading to: ', response.data.uploadURL)
-      const result = await fetch(response.data.uploadURL, {
-        method: 'PUT',
-        body: blobData
-      })
-      console.log('Result: ', result)
-      // Final URL for the user doesn't need the query string params
-      setUploadUrl(response.data.uploadURL.split('?')[0]);
-      setFile('');
+    });
+    console.log("Response: ", response.data);
+    console.log("Uploading: ", file);
+    let binary = atob(file.split(",")[1]);
+    let array = [];
+    for (var i = 0; i < binary.length; i++) {
+      array.push(binary.charCodeAt(i));
     }
-  ;
+    let blobData = new Blob([new Uint8Array(array)], { type: "text/plain" });
+    console.log("Uploading to: ", response.data.uploadURL);
+    const result = await fetch(response.data.uploadURL, {
+      method: "PUT",
+      body: blobData
+    });
+    console.log("Result: ", result);
+    // Final URL for the user doesn't need the query string params
+    setUploadUrl(response.data.uploadURL.split("?")[0]);
+    setFile("");
+  };
 
   return (
     <div className={classes.content}>
@@ -72,7 +74,7 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
         {title}
       </Typography>
       {!file ? (
-          <input type="file" onChange={onFileChange}/>
+        <input type="file" onChange={onFileChange} />
       ) : (
         <div>
           {!uploadUrl && <button onClick={removeFile}>Remove file</button>}
